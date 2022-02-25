63dcd9fbec522db9625f711ba1d8a6f88ce99f8c
kuraiokamiblaze <kuraiokamiblaze@penguin>
2022-02-25 12:42:35 -0700

    index.js set up

--- src/App.js
+++ src/App.js
@@ -3,8 +3,28 @@ import React from "react";
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <div id="container-header">
+        <h1>Lambda Eats</h1>
+        {/* <link>Home</link>
+        <link>PizzaOrder</link> */}
+      </div>
+      
+      <div id="container-image">
+        {/* img of pizza */}
+        <p>Where Coders Are Our Key Customers</p>
+      </div>
+
+      <div id="container-pizzaText">
+        <p>We know how hard it can be after a long session of coding.Let's be honest...you're normally starving but the last thing you want to do is cook. Well let us cater to you! Our amazing pizzas and toppings were picked specifically with coders in mind. So send us an order below</p>
+      </div>
+
+      <div id="container-order">
+        <button>Order Pizza</button>
+      </div>
+
+      <div id="container-footer">
+        {/* <link>Back to the Top</link> */}
+      </div>
     </>
   );
 };
